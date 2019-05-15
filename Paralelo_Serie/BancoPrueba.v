826bc39678c15ccd77f70944378bebbae45faba0
pvargasj <pablo.vargasjimenez@ucr.ac.cr>
2019-05-14 18:19:26 -0600

    Agregando la sintesis estructural del paralelo serie

--- Paralelo_Serie/BancoPrueba.v
+++ Paralelo_Serie/BancoPrueba.v
@@ -4,6 +4,7 @@
 //includes de archivos
 `include "Paralelo_Serie_conductual.v"
 `include "probador.v"
+`include "P_S_Yosys.v"
  
 module BancoPruebas; // Testbench
 
@@ -12,8 +13,11 @@ module BancoPruebas; // Testbench
 	wire		clk_8f;			// From probador_ of probador.v
 	wire		clk_f;			// From probador_ of probador.v
 	wire [7:0]	data2send_c;		// From P_S_Cond of Paralelo_Serie_conductual.v
+	wire [7:0]	data2send_e;		// From P_S_synth of P_S_Yosys.v
 	wire [7:0]	data_in;		// From probador_ of probador.v
 	wire		data_out_c;		// From P_S_Cond of Paralelo_Serie_conductual.v
+	wire		data_out_e;		// From P_S_synth of P_S_Yosys.v
+	wire		reset;			// From probador_ of probador.v
 	wire		valid_in;		// From probador_ of probador.v
 	// End of automatics
 	
@@ -26,10 +30,20 @@ module BancoPruebas; // Testbench
 						 .clk_8f		(clk_8f),
 						 .clk_f			(clk_f),
 						 .data_in		(data_in[7:0]),
-						 .valid_in		(valid_in));
+						 .valid_in		(valid_in),
+						 .reset			(reset));
 
-	// Descripción sintetizada de demux
-	//proy1_synth		p_synth(/*AUTOINST*/);
+	// Descripción sintetizada del paralelo serie.
+	P_S_Yosys	P_S_synth(/*AUTOINST*/
+				  // Outputs
+				  .data2send_e		(data2send_e[7:0]),
+				  .data_out_e		(data_out_e),
+				  // Inputs
+				  .clk_8f		(clk_8f),
+				  .clk_f		(clk_f),
+				  .data_in		(data_in[7:0]),
+				  .reset		(reset),
+				  .valid_in		(valid_in));
 
 	// Probador: generador de señales y monitor
 	probador probador_(/*AUTOINST*/
@@ -38,8 +52,13 @@ module BancoPruebas; // Testbench
 			   .valid_in		(valid_in),
 			   .clk_f		(clk_f),
 			   .clk_8f		(clk_8f),
+			   .reset		(reset),
 			   // Inputs
 			   .data_out_c		(data_out_c),
-			   .data2send_c		(data2send_c[7:0]));
+			   .data2send_c		(data2send_c[7:0]),
+			   .data_out_e		(data_out_e),
+			   .data2send_e		(data2send_e[7:0]));
+
+
 	
 endmodule
--- Paralelo_Serie/Paralelo_Serie_conductual.v
+++ Paralelo_Serie/Paralelo_Serie_conductual.v
@@ -1,25 +1,39 @@
-module Paralelo_Serie_conductual(input clk_8f,
-                      input clk_f,
-                      input [7:0] data_in,
-                      input valid_in,
-                      output reg [7:0] data2send_c,
-                      output reg data_out_c);
-    reg [2:0] j = 0;
+`include "CMOS_cells.v" 
+
+module Paralelo_Serie_conductual(input  clk_8f,
+                                 input  clk_f,
+                                 input  [7:0] data_in,
+                                 input  valid_in,
+                                 input  reset,
+                                 output reg [7:0] data2send_c,
+                                 output reg  data_out_c);
+    reg [2:0] j;
+    wire [7:0] data_in;
+    wire valid_in, clk_8f, clk_f;
+
 
     always @(*)begin
+        if(reset) begin
+            data_out_c = 0;
+        end
+        else
         data_out_c = data2send_c[j];
    end
 
    always @(posedge clk_8f) begin
-        j-=1;
+        if (!reset) begin
+            j <= j-1;
+        end
+        else
+        j <=3'b111;
    end
 
-    always @(posedge clk_f) begin
+    always @(*) begin
         if(valid_in) begin
-            data2send_c <= data_in;
+            data2send_c = data_in;
         end
         else begin
-            data2send_c <= 16'hbc;
+            data2send_c = 16'hbc;
         end
 
     end
